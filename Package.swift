05be08a09094e44803a969e938b1e5c4ec53c736
jmclin <jmclin@envoc.com>
2024-08-12 15:51:50 -0500

    chore: bump to latest release

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "PBJTVStreaming.iOS",
-            url: "https://github.com/jmclin96/PBJTVStreaming.iOS/raw/1.0.2/PBJTVStreaming.xcframework.zip",
+            url: "https://github.com/jmclin96/PBJTVStreaming.iOS/raw/1.0.3/PBJTVStreaming.xcframework.zip",
             checksum: "3f4d64b2426aacb5e222c460258a9d42bae66b9b684fbec8ce725017aa50378c"
         ),
     ]
