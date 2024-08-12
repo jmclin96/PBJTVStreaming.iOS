2f70741c0725ab2a7714121158410a98fee473d8
jmclin <jmclin@envoc.com>
2024-08-12 13:48:13 -0500

    feat: adding swift package with binary reference

---
+++ PBJTVStreaming.iOS/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PBJTVStreaming.iOS",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "PBJTVStreaming.iOS",
+            targets: ["PBJTVStreaming.iOS"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "PBJTVStreaming.iOS",
+            url: "https://github.com/jmclin96/PBJTVStreaming.iOS/raw/main/PBJTVStreaming.xcframework.zip",
+            checksum: "24e971ae4c08783dca3af29b50c18d90c808e92a730355c1abbdb559058d7906"
+        ),
+    ]
+)
