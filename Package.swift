44a7dd8e355ac7ee72e0e988538530fcaab55fc3
jmclin96 <runner@Mac-1723570813584.local>
2024-08-13 17:46:32 +0000

    feat: DevOps SDK Deployment Pipeline

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "PBJTVStreaming.iOS",
-            url: "https://github.com/jmclin96/PBJTVStreaming.iOS/raw/main/PBJTVStreaming.xcframework.zip?v=39194030",
-            checksum: "f6a54abd4fd83aebe19f0736382d0c6d8ca73530eed9a0e4c9e6c719afe03053"
+            url: "https://github.com/jmclin96/PBJTVStreaming.iOS/raw/main/PBJTVStreaming.xcframework.zip?v=94006294",
+            checksum: "7651c07681821f62ccbbcaeaab62486b12fc89bdbfc79b0a7a7cf9937e46107d"
         ),
     ]
 )
