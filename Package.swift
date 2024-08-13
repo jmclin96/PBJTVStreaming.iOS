7d546a8d117d29cbcabccf67a5c47c632b2f7230
jmclin96 <runner@Mac-1723561716562.local>
2024-08-13 15:13:21 +0000

    feat: DevOps SDK Deployment Pipeline Step 2

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "PBJTVStreaming.iOS",
             url: "https://github.com/jmclin96/PBJTVStreaming.iOS/raw/main/PBJTVStreaming.xcframework.zip?v=1.0.4",
-            checksum: "788a06cabd1e9c375f44d4b4f803a84774e5c7d7a57c6e49e1010bfa54c7e5bb"
+            checksum: "aede6d4b5d35480f68206573933b99d437f40bc6558d3cccdd9787974128f35d"
         ),
     ]
 )
